db3081b15865ed85424c8932556ff4411c8cc4b1
yn1323 <yn1323@gmail.com>
2021-08-01 23:55:36 +0900

    feat: small cards

---
+++ components/organism/IllustCardsSmall/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import IllustCardSmall from '@template/IllustCardSmall'
+import styled from 'styled-components'
+import tw from 'tailwind-extended.macro'
+
+const illusts = [
+  {
+    isGray: false,
+    imageUri: '/images/functionalIcon1.png',
+    text: '開閉店時間',
+  },
+  {
+    isGray: true,
+    imageUri: '/images/functionalIcon2.png',
+    text: 'タイムカード',
+  },
+  {
+    isGray: false,
+    imageUri: '/images/functionalIcon3.png',
+    text: '月ごとで管理',
+  },
+  {
+    isGray: true,
+    imageUri: '/images/functionalIcon4.png',
+    text: '勤務時間のリスト化',
+  },
+  {
+    isGray: false,
+    imageUri: '/images/functionalIcon5.png',
+    text: '無料',
+  },
+  {
+    isGray: true,
+    imageUri: '/images/functionalIcon6.png',
+    text: 'クラウドに保存',
+  },
+] as const
+
+const Root = styled.div`
+  ${tw`flex flex-wrap`}
+`
+
+const IllustCardsSmall: React.FC = () => {
+  const illustCardArray = illusts.map(({ isGray, imageUri, text }, i) => (
+    <IllustCardSmall key={i} isGray={isGray} imageUri={imageUri} text={text} />
+  ))
+  return <Root>{illustCardArray}</Root>
+}
+
+export default IllustCardsSmall
