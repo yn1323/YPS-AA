d8e160ee5f8857e41d63956b754ca416c37d9e93
yn1323 <yn1323@gmail.com>
2021-08-30 00:38:27 +0900

    delete: unnecessary component

--- components/atom/BurgerButton/index.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { mountWithTheme } from '@test/TestWrapper'
-import React from 'react'
-import Component from '.'
-
-test(`${Component.name}-snapshot`, () => {
-  const wrapper = mountWithTheme(<Component />)
-  expect(wrapper).toMatchSnapshot()
-})
