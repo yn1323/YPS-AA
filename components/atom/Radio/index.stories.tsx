b8903b11425f2d3cacd39325e5d835435b353eb1
yn1323 <yn1323@gmail.com>
2021-09-12 02:37:03 +0900

    fix: args

--- components/atom/Radio/index.stories.tsx
+++ components/atom/Radio/index.stories.tsx
@@ -2,26 +2,24 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 import React from 'react'
 import Component, { Props } from '.'
 
-const Args: Props = {
-  groupName: 'age',
-  initialVal: '200',
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  setter: v => {},
-  options: [
-    { value: '100', label: 'hundred' },
-    { value: '200', label: 'two-hundred' },
-    { value: '300', label: 'disabled', disabled: true },
-    { value: '400', label: 'disabled = false', disabled: false },
-    { value: '500', label: 'color-default', color: 'default' },
-    { value: '600', label: 'color-secondary', color: 'secondary' },
-  ],
-  row: true,
-}
-
 export default {
   title: `atom/${Component.name}`,
   component: Component,
-  args: Args as Props,
+  args: {
+    groupName: 'age',
+    initialVal: '200',
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    setter: v => {},
+    options: [
+      { value: '100', label: 'hundred' },
+      { value: '200', label: 'two-hundred' },
+      { value: '300', label: 'disabled', disabled: true },
+      { value: '400', label: 'disabled = false', disabled: false },
+      { value: '500', label: 'color-default', color: 'default' },
+      { value: '600', label: 'color-secondary', color: 'secondary' },
+    ],
+    row: true,
+  } as Props,
 } as ComponentMeta<typeof Component>
 
 const Template: ComponentStory<typeof Component> = args => (
