6f3b35f5f2b41e15721d29d5d2acd4add66ca873
yn1323 <yn1323@gmail.com>
2021-09-15 21:57:21 +0900

    fix: heading

--- components/atom/Text/Heading/index.tsx
+++ components/atom/Text/Heading/index.tsx
@@ -1,19 +1,24 @@
-import { Typography } from '@material-ui/core'
+import { Typography, TypographyProps } from '@material-ui/core'
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import tw from 'tailwind-extended.macro'
 
-const HeaderTypes = { sub: 'h2' } as const
+type Types = 'sub' | 'description'
+const HeaderTypes = { sub: 'h2', description: 'subtitle1' } as const
 
 interface Props {
   children: string
-  type: keyof typeof HeaderTypes
+  type: Types
   showUnderline?: boolean
 }
 
 const styles = {
   sub: styled(Typography)`
     font-size: 2rem;
+    color: ${({ theme }) => theme.palette.text.primary};
+  `,
+  description: styled(Typography)`
+    font-size: 1rem;
     color: ${({ theme }) => theme.palette.text.primary};
   `,
 }
@@ -23,6 +28,7 @@ const underlines = {
     ${tw`h-0.5`}
     background-image: linear-gradient(to right, #ac5d79, #6d546c, #444444);
   `,
+  description: styled.div``,
 }
 
 const Heading: FC<Props> = ({ children, type, showUnderline }) => {
