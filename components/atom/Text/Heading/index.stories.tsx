fb640fcb7fae5916930100ae330a8cb481bb9d41
yn1323 <yn1323@gmail.com>
2021-10-02 02:03:45 +0900

    feat: heading with icon & link

--- components/atom/Text/Heading/index.stories.tsx
+++ components/atom/Text/Heading/index.stories.tsx
@@ -1,6 +1,8 @@
+import { AccessTime } from '@material-ui/icons'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import React from 'react'
 import Component from '.'
+import { theme } from '@constant/theme'
 
 export default {
   title: `atom/Text/Heading`,
@@ -16,3 +18,8 @@ const Template: ComponentStory<typeof Component> = args => (
 )
 
 export const Basic = Template.bind({})
+export const WithIcon = Template.bind({})
+WithIcon.args = {
+  icon: <AccessTime style={{ color: theme.palette.text.secondary }} />,
+  link: 'link',
+}
--- components/atom/Text/Heading/index.tsx
+++ components/atom/Text/Heading/index.tsx
@@ -1,4 +1,5 @@
-import { Typography, TypographyProps } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import { Link } from '@material-ui/icons'
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import tw from 'tailwind-extended.macro'
@@ -9,8 +10,24 @@ const HeaderTypes = { sub: 'h2', description: 'subtitle1' } as const
 interface Props {
   children: string | JSX.Element
   type: Types
+  icon?: JSX.Element
+  link?: string
   underline?: boolean
 }
+
+const Container = styled.div`
+  ${tw`flex items-center`}
+`
+const Header = styled.div`
+  ${tw`inline-block`}
+`
+const HeaderWrapper = styled.div`
+  ${tw`pl-1 pr-12 flex items-center`}
+`
+const LinkIcon = styled(Link)`
+  ${tw`ml-4`}
+  color: ${({ theme }) => theme.palette.text.secondary};
+`
 
 const styles = {
   sub: styled(Typography)`
@@ -25,21 +42,28 @@ const styles = {
 
 const underlines = {
   sub: styled.div`
-    ${tw`h-0.5`}
-    background-image: linear-gradient(to right, #da8d00d5, #ffa6006a, #ffa60016);
+    ${tw`h-0.5 w-full`}
+    background-image: linear-gradient(to right, #da8d00d5, #ffa6006a, #ffa60018);
   `,
   description: styled.div``,
 }
 
-const Heading: FC<Props> = ({ children, type, underline }) => {
+const Heading: FC<Props> = ({ children, type, underline, icon, link }) => {
   const variant = HeaderTypes[type]
   const HeaderElem = styles[type]
   const Underline = underlines[type]
+
   return (
-    <>
-      <HeaderElem variant={variant}>{children}</HeaderElem>
-      {underline && <Underline />}
-    </>
+    <Container onClick={() => console.log(link)}>
+      {icon}
+      <Header>
+        <HeaderWrapper>
+          <HeaderElem variant={variant}>{children}</HeaderElem>
+          {!!link && <LinkIcon />}
+        </HeaderWrapper>
+        <div>{underline && <Underline />}</div>
+      </Header>
+    </Container>
   )
 }
 
